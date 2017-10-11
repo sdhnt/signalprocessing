d9a3f3aeb9512a430843dff08bde06de5518b231
Sidhant Gupta <sdhnt@connect.hku.hk>
2017-10-11 12:11:30 +0800

    Add files via upload

--- LabSid.m
+++ LabSid.m
@@ -9,23 +9,30 @@ f=4000;
 tm1 = (37.2/3)* T; %<--- 12.4
 tm2 = -(41.3/8)*T;  %<---  -5.1625
 x1 = A1*exp(j*2*pi*f*(tt - tm1));
+x11 = A1*exp(j*2*pi*f*(tt))*exp(j*2*pi*f*(-tm1)); %<-- CALCULATE AND COMPLETE
 x2 = A2*exp(j*2*pi*f*(tt - tm2));
 x3= x1 + x2;
 
-subplot(3,1,1);  title('Sidhant Gupta x1(t)');
+subplot(4,1,1);  title('Sidhant Gupta x1(t)');
 plot( tt,x1, 'b-'), grid on
 %<--- plot a sinusoid
 title('TEST PLOT')
 xlabel('TIME (sec)')
 
-subplot(3,1,2);  title('Sidhant Gupta x2(t)');
+subplot(4,1,2);  title('Sidhant Gupta x1(t): Single Exponent');
+plot( tt,x11, 'b-'), grid on
+%<--- plot a sinusoid
+title('TEST PLOT')
+xlabel('TIME (sec)')
+
+subplot(4,1,3);  title('Sidhant Gupta x2(t)');
 
 plot(tt, x2, 'r--'), grid on
 %<--- plot a sinusoid
 title('TEST PLOT')
 xlabel('TIME (sec)')
 
-subplot(3,1,3);  title('Sidhant Gupta x3(t)');
+subplot(4,1,4);  title('Sidhant Gupta x3(t)');
 plot(tt, x3, 'g--'), grid on
 %<--- plot a sinusoid
 title('TEST PLOT')
